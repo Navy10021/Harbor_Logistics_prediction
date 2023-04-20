5733982941efb23f7b38b7673b3593b3f366e4a2
Yoonseop Lee <navy10021@snu.ac.kr>
2023-04-20 13:01:42 +0900

    Create scenario.py

---
+++ scenario.py
@@ -0,0 +1,24 @@
+# 1.Data Analysis
+  1. 연도별 주요 항만 물류량
+  2. 월별 패턴 확인
+  3. 월별 평균 확인
+
+# 2.Time Series Data
+  1. Time dependency
+     - 월/분기/연도별 패턴 확인
+        * Store Sales TS Forcasting 참고
+        * Time Series 참고
+     - Linear Prediction Model
+  2. Series dependency
+     - Linear Prediction Model
+
+# 4. Feature Creation / Feature importance
+  - Corleation-based : 선형관계로 Feature의 중요도 판단하는 방법
+  - Ensemble-based : Feature가 앙상블 모델 예측에 어느정도 영향을 미쳤는지 측정하여 중요도를 판단하는 방법
+    * 앙상블 기법 : 어려개의 분류기(의사결정 나무)를 생성하고, 각 모델들의 예측값을 결합함으로써 
+      보다 정확한 예측을 도출하는 기계학습 방법론
+      
+# Visualize Features & Target
+
+# Create Transformer-based prediction model
+
